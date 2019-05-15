b7d8830f22b2e5b6cd989e6e392417106d83c581
UganderD <50668958+UganderD@users.noreply.github.com>
2019-05-15 20:31:11 +0530

    Committed

--- overrides/0.9.0.1.sh
+++ overrides/0.9.0.1.sh
@@ -3,4 +3,4 @@
 # Kafka 0.9.x.x has a 'listeners' config by default. We need to remove this
 # as the user may be configuring via the host.name / advertised.host.name properties
 echo "Removing 'listeners' from server.properties pre-bootstrap"
-sed -i -e '/^listeners=/d' "$KAFKA_HOME/config/server.properties"
+sed -i -e '/^listeners=/d' "$KAFKA_HOME/config/server.properties"  
